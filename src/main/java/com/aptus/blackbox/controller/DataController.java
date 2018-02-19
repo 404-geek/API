f90ff33bd4dc40055613dd59f3cd846c0baf220e
souravdas28 <sourav.swayambhu@aptusdatalabs.com>
2018-02-19 12:02:01 +0530

    Updated with data controller

--- src/main/java/com/aptus/blackbox/controller/DataController.java
+++ src/main/java/com/aptus/blackbox/controller/DataController.java
@@ -1,6 +1,13 @@
 package com.aptus.blackbox.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +18,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aptus.blackbox.Service.Credentials;
+import com.aptus.blackbox.index.DestObject;
+import com.github.opendevl.JFlat;
 
 @RestController
 public class DataController {
+	private String mongoUrl,connectionID;
+	private Connection con=null;
 	
-	private String mongoUrl;
 	
 	public DataController(Environment env) {
 		mongoUrl = env.getProperty("spring.mongodb.ipAndPort");
@@ -35,11 +45,122 @@ public class DataController {
 			credentials.setDestToken(destCred);
 			 
 			
-	    }
+			if(!checkDB(credentials.getDestToken().get("database_name")))
+			{
+				//invalid
+			}
+		  }
 		catch(Exception e) {
 			e.printStackTrace();
 		}
-		return;
+}
+	
+	public boolean jsontodatabase(String jsonString) throws SQLException
+	{
+		
+		try {
+			
+			JFlat x = new JFlat(jsonString);
+			List<Object[]> json2csv = x.json2Sheet().getJsonAsSheet();
+			Map<String,String> dt = credentials.getDestToken();
+			String host = dt.get("server_host");
+			String port = dt.get("server_port");
+			String dbase = dt.get("database_name");
+			String user = dt.get("db_username");
+			String pass = dt.get("db_password");	
+			
+			if (checkDB(credentials.getDestToken().get("database_name"))) {
+				String statement =  "CREATE TABLE `" + connectionID + "`(";
+				int i = 0;
+				int count=0;
+				
+				for (Object[] o : json2csv) {
+					if (i > -1) {
+						if(con==null)
+							connection();
+						if (i == 0) {
+							for (Object t : o) {
+								statement += "`" + t.toString() + "` TEXT," ;
+							}
+							System.out.println(statement.substring(0, statement.length() - 1) + ");");
+							PreparedStatement stmt=con.prepareStatement(statement.substring(0, statement.length() - 1) + ");");  
+							stmt.executeUpdate();
+						} else {
+							String insstmt = "INSERT INTO `"+connectionID+"` VALUES(";
+							for (Object t : o) {
+								if (t == null) {
+									insstmt +=""
+											+ "NULL ,";
+								} else {
+									count++;
+									insstmt+=t.toString()+",";
+								}
+								insstmt=insstmt.substring(0,insstmt.length()-1)+"(";
+								for(int j=0;j<count;j++)
+								{
+									insstmt+="?,";
+								}
+								
+							}
+							PreparedStatement stmt=con.prepareStatement(insstmt.substring(0, statement.length() - 1) + ");");
+							System.out.println(insstmt.substring(0,insstmt.length()-1)+");");
+							stmt.executeUpdate();
+						}
+					}
+					i = i + 1;
+				}
+				}
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		finally {if(con==null)
+			connection();
+			con.close();
+		}
+	}
+	public void connection() throws SQLException
+	{
+		try {
+			DestObject destObj=credentials.getDestObj();
+			Class.forName(destObj.getDrivers());
+			String url = destObj.getUrlprefix() + credentials.getDestToken().get("server_host")
+					+":" + credentials.getDestToken().get("server_port")+  
+					destObj.getDbnameseparator() + credentials.getDestToken().get("database_name");
+			con = DriverManager.getConnection(url, credentials.getDestToken().get("db_username"), credentials.getDestToken().get("db_password"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean checkDB(String dbase) throws SQLException
+	{
+		
+		try {
+			if(con==null)
+				connection();
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT count(*) FROM information_schema.tables WHERE table_schema =" + "'"
+					+ dbase + "'" + " AND table_name =" + "'" + connectionID + "'");
+			while (rs.next()) {
+				System.out.println(rs.getInt(1));
+				if (rs.getInt(1) == 1) {
+					stmt.executeUpdate("DROP " + "`" + connectionID + "`");
+					con.close();
+					return true;
+				}
+			}
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
 	}
 	
 }
