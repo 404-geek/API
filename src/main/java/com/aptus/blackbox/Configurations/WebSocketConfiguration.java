d49bcac1b96c7ac7b52f6696985f4f8dc4193a5b
code2hackon <chandanbera996@gmail.com>
2018-08-28 12:22:33 +0530

    Cors Config

--- src/main/java/com/aptus/blackbox/Configurations/WebSocketConfiguration.java
+++ src/main/java/com/aptus/blackbox/Configurations/WebSocketConfiguration.java
@@ -1,4 +1,4 @@
-package com.aptus.blackbox.socketConfiguration;
+package com.aptus.blackbox.Configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
